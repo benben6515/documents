756c5954dc325f095c89e4c7f2b267ba3b674158
benben <z2266109@gmail.com>
2023-04-05 10:25:21 +0800

    feat(example): update react, vue atomic css examples

--- examples/atomic-css-in-components/react.tsx
+++ examples/atomic-css-in-components/react.tsx
@@ -1,7 +1,31 @@
 import React, { useState } from 'react'
 
+// import global utils css string
+import { globalSectionStyle } from '@/styles/global'
+
+// utils css string
+const localTitleStyle = 'text-red-400 p-2'
+
+// TIPS: also you using atomic css like style components, but just put they in `className`.
+const flexCenter = 'flex justify-center items-center'
+const buttonStyle = `
+  text-teal-400
+  text-bold
+  ${flexCenter}
+`
+
 const Component = () => (
-  <section>
-    {/* TODO: */}
-  </section>
+  <div>
+    {/* global css with some class names */}
+    <section className="`${globalSectionStyle} bg-red-400 flex-col`">
+      {/* local css */}
+      <h3 className="${localTitleStyle}">Section 1</h3>
+    </section>
+
+    {/* global css with some class names */}
+    <section className="`${globalSectionStyle} bg-red-400">
+      {/* local css with some class names */}
+      <h3 className="`${localTitleStyle} bg-blue-300">Section 2</h3>
+    </section>
+  </div>
 )
--- examples/atomic-css-in-components/vue.vue
+++ examples/atomic-css-in-components/vue.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
+// import global utils css string
 import { globalSectionStyle } from '@/styles/global'
-// 'p-2 border flex'
 
+// utils css string
 const localTitleStyle = 'text-red-400 p-2'
 
 </script>
 
 <template>
   <div>
+    <!-- global css with some class names -->
     <section :class="globalSectionStyle" class="bg-red-400 flex-col">
+      <!-- local css -->
       <h3 :class="localTitleStyle">Section 1</h3>
     </section>
 
+    <!-- global css with some class names -->
     <section :class="globalSectionStyle" class="bg-red-400">
+      <!-- local css with some class names -->
       <h3 :class="localTitleStyle" class="bg-blue-300">Section 2</h3>
     </section>
   </div>
